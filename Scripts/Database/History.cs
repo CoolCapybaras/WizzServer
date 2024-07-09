56aa4281d61e2b85e3916adfb7bf97e5c78db7b1
LeonKote <roman892265@gmail.com>
2024-07-09 23:02:08 +0500

    Implemented history of played quizzes

---
+++ Scripts/Database/History.cs
@@ -0,0 +1,11 @@
+﻿namespace WizzServer.Database
+{
+	public class History
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public int QuizId { get; set; }
+		public Quiz Quiz { get; set; }
+		public DateTimeOffset CompleteDate { get; set; }
+	}
+}
